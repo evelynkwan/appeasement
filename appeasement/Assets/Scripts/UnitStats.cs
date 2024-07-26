a48925c45481a94bfd25a693b5807cf24b9f1426
Buluman321 <111328047+Buluman321@users.noreply.github.com>
2024-07-26 14:45:27 -0400

    Added upgrade files

--- appeasement/Assets/Scripts/UnitStats.cs
+++ appeasement/Assets/Scripts/UnitStats.cs
@@ -14,11 +14,15 @@ public class UnitStats : MonoBehaviour
     public int meterRate;
     public int specialattack;
     public bool dead = false;
+    private bool first = true;
     // Start is called before the first frame update
 
     void Start()
     {
-        
+        if (first)
+        {
+            first = false;
+        }
     }
 
     // Update is called once per frame
